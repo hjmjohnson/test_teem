abd08c279f696e649383834f2aa772b42b438486
nobody <nobody@3d70eeeb-363e-0410-a505-8a46323a89f2>
2005-12-09 20:14:04 +0000

    This commit was manufactured by cvs2svn to create tag 'start'.

---
+++ Deft/tags/start/src/Object.h
@@ -0,0 +1,120 @@
+/*
+  Deft: experiments in minimalist scientific visualization 
+  Copyright (C) 2005  Gordon Kindlmann
+
+  Permission is hereby granted, free of charge, to any person obtaining
+  a copy of this software and associated documentation files (the
+  "Software"), to deal in the Software without restriction, including
+  without limitation the rights to use, copy, modify, merge, publish,
+  distribute, sublicense, and/or sell copies of the Software, and to
+  permit persons to whom the Software is furnished to do so, subject to
+  the following conditions:
+
+  The above copyright notice and this permission notice shall be
+  included in all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+  LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+  WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+/*
+** learned: DO NOT forget the ";" at the end of a class declaration, or
+** else you'll get totally cryptic: "expected unqualified-id before ..."
+** compile errors...
+*/
+
+#ifndef DEFT_OBJECT_INCLUDED
+#define DEFT_OBJECT_INCLUDED
+
+#include "Deft.H"
+
+namespace Deft {
+
+extern const unsigned int displayListDrawCount;
+
+class DEFT_EXPORT Object;
+
+typedef void (Callback)(Object *, void *);
+
+/*
+******** class Object
+**
+** the superclass for all things that are drawn to the screen
+*/
+class DEFT_EXPORT Object {
+public:
+  explicit Object();
+  virtual ~Object();
+
+  // the draw() method handles display lists
+  void draw();
+  virtual void drawImmediate()=0;
+
+  // For learning the axis-aligned bounding box
+  virtual void boundsGet(float min[3], float max[3]) const =0;
+
+  // To indicate that any cached displayList should be nixed.
+  // It is the Object user's job to indicate when its contents have changed...
+  void changed();
+
+  // For when we're merely a vessel for compiling a display last; 
+  // the display list ID is returned, and we do not "own" it
+  unsigned int compileDisplayList();
+
+  // Setting and getting various booleans
+  void wireframe(bool);
+  bool wireframe() const;
+  void twoSided(bool);
+  bool twoSided() const;
+  void visible(bool);
+  bool visible() const;
+  void colorUse(bool);
+  bool colorUse() const;
+  void normalizeUse(bool);
+  bool normalizeUse() const;
+  void compilingUse(bool);
+  bool compilingUse() const;
+  void lightingUse(bool);
+  bool lightingUse() const;
+  void transformUse(bool);
+  bool transformUse() const;
+
+  // Setting and getting the transform
+  void transformSet(const float mat[16]);
+  void transformGet(float mat[16]) const;
+
+  double drawTime() const;
+
+  // callbacks
+  void postDrawCallback(Callback *cb, void *data);
+protected:
+  // for compiling display list when we're speeding up drawing;
+  // we do own the resulting display list
+  void compile();
+  bool _compiling;
+  unsigned int _staticDrawCount;
+  unsigned int _displayList;
+
+  // internal booleans
+  bool _wireframe, _twoSided, _visible, _colorUse, _normalizeUse,
+    _compilingUse, _lightingUse, _transformUse;
+
+  // transform applied to whole object
+  float _transform[16];
+
+  // time elapsed during drawing
+  double _drawTime;
+
+  // callbacks
+  Callback *_postDrawCallback;
+  void *_postDrawCallbackData;
+};
+
+}
+
+#endif /* DEFT_OBJECT_INCLUDED */
---
+++ Deft/tags/start/src/Scene.h
@@ -0,0 +1,87 @@
+/*
+  Deft: experiments in minimalist scientific visualization 
+  Copyright (C) 2005  Gordon Kindlmann
+
+  Permission is hereby granted, free of charge, to any person obtaining
+  a copy of this software and associated documentation files (the
+  "Software"), to deal in the Software without restriction, including
+  without limitation the rights to use, copy, modify, merge, publish,
+  distribute, sublicense, and/or sell copies of the Software, and to
+  permit persons to whom the Software is furnished to do so, subject to
+  the following conditions:
+
+  The above copyright notice and this permission notice shall be
+  included in all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+  LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+  WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#ifndef DEFT_SCENE_INCLUDED
+#define DEFT_SCENE_INCLUDED
+
+#include "Deft.H"
+#include "Actor.H"
+#include "Object.H"
+#include "Group.H"
+
+namespace Deft {
+
+class DEFT_EXPORT Scene {
+public:
+  // constructor/destructor
+  explicit Scene();
+  ~Scene();
+
+  // set/get background color
+  void bgColor(float R, float G, float B);
+  float bgColorR();
+  float bgColorG();
+  float bgColorB();
+
+  // set/get ambient light
+  void ambLight(float R, float G, float B);
+  float ambLightR();
+  float ambLightG();
+  float ambLightB();
+  // update (viewspace) lights
+  void lightUpdate(limnCamera *cam);
+
+  // set objects in scene
+  int actorAdd(Actor *actor);
+  Actor *actorRemove(int actorIdx);
+
+  int objectAdd(Object *obj);
+  Object *objectRemove(int objectIdx);
+
+  int groupAdd(Group *obj);
+  Group *groupRemove(int groupIdx);
+
+  // draw the scene
+  void draw();
+  double drawTime() const { return _drawTime; }
+  double totalTime() const { return _totalTime; }
+
+  // find bounding box for everything in scene
+  void boundsGet(float min[3], float max[3]) const;
+
+private:
+  Actor **actor;
+  airArray *actorArr;
+  Object **object;
+  airArray *objectArr;
+  Group **group;
+  airArray *groupArr;
+  float _bgColor[3];
+  double _lastTime, _totalTime, _drawTime;
+  limnLight *lit;
+};
+
+}
+
+#endif /* DEFT_SCENE_INCLUDED */
